1ea1eabb6b2f476a21a093397ebea86b7a2b63cd
Abhilasha Lahare <abhilashalahare6@gmail.com>
2026-03-06 11:46:03 +0530

    backend updated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
@@ -25,5 +28,5 @@ app.use('/api/ai', aiRoutes);
 
 app.get('/', (req, res) => res.send('Poster Maker API is running...'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
